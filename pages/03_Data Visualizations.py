297141a6be738461ffc2b4b28e457196e3559407
Kabeer Bhagat <84463158+beersk1@users.noreply.github.com>
2023-08-22 20:06:07 +0530

    Delete pages directory

--- pages/03_Data Visualizations.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Read the HTML content from the file
-with open(r".\my_report.html", "r") as file:
-    html_content = file.read()
-
-# Display the HTML content
-st.components.v1.html(html_content, width=1000, height=1500)
